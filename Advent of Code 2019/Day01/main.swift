71507638eb192898fffa90cbec626e4597291cf2
Michael Fenske <michael@codertrix.com>
2019-12-01 17:01:31 +0100

    Day 1: The Tyranny of the Rocket Equation

---
+++ Advent of Code 2019/Day01/main.swift
@@ -0,0 +1,39 @@
+// ---------------------------------------------------
+//  main.swift
+//  Advent of Code 2019
+//
+//  Copyright © 2019 Michael Fenske. See LICENSE.txt.
+// ---------------------------------------------------
+
+import Foundation
+
+
+// --------------------
+//   MARK: - Puzzle 1
+// --------------------
+
+var sumOfFuel = 0
+
+for mass in masses {
+    sumOfFuel += mass / 3 - 2
+}
+
+print("The sum of fuel required is \(sumOfFuel)")
+
+
+// --------------------
+//   MARK: - Puzzle 2
+// --------------------
+
+sumOfFuel = 0
+
+for var mass in masses {
+    mass = mass / 3 - 2
+
+    while mass > 0 {
+        sumOfFuel += mass
+        mass = mass / 3 - 2
+    }
+}
+
+print("The sum of fuel required is \(sumOfFuel)")
